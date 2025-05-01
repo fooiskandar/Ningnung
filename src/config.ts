f5061f23bacdda1deb6a53c554cd459431edba70
fooiskandar <fooiskandar@gmail.com>
2025-05-01 12:34:25 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'Awan Zone ID',
+  subtitle: 'Semua Tentang Musik dan Lirik.',
   lang: 'id',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
