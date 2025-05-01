9ff6dbdfb33912ee4db760c6cae6e77c9985e50d
fooiskandar <fooiskandar@gmail.com>
2025-05-01 13:13:57 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Awan Zone ID',
+  bio: 'Mengenal dan mengetahui apa itu.',
   links: [
     {
       name: 'Twitter',
