f0ba9d9f575c8a7918785e3cf9cc417eecc51565
fooiskandar <fooiskandar@gmail.com>
2025-05-01 10:38:21 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://awan.zone.id/",
   base: "/",
   trailingSlash: "always",
   integrations: [
